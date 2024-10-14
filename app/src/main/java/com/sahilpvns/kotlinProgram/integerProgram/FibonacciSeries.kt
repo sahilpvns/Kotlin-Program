4fa7b0efdfbc12f7d9a94f71c0d06da291a99146
sahil <sahilpvns@gmail.com>
2024-10-14 10:03:12 +0530

    Program

--- app/src/main/java/com/sahilpvns/kotlinProgram/integerProgram/FibonacciSeries.kt
+++ app/src/main/java/com/sahilpvns/kotlinProgram/integerProgram/FibonacciSeries.kt
@@ -5,12 +5,12 @@ fun main() {
     val n = 10
     var t1 = 0
     var t2 = 1
-    print("First $n terms: ")
     for (i in 1..n) {
-        print("$t1 + ")
         val sum = t1 + t2
         t1 = t2
         t2 = sum
+
+        print("$t1 + ")
     }
 }
 
