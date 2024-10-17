210fb24172ab2d03bcdcd79d7400ba35c4be547c
sahil <sahilpvns@gmail.com>
2024-10-17 21:36:01 +0530

    Program

--- app/src/main/java/com/sahilpvns/kotlinProgram/arrayProgram/LargestElementArray.kt
+++ app/src/main/java/com/sahilpvns/kotlinProgram/arrayProgram/LargestElementArray.kt
@@ -2,11 +2,11 @@ package com.sahilpvns.kotlinProgram.arrayProgram
 
 // Program to find largest element in an Array
 fun main() {
-    val arr = arrayOf(5, 2, 9, 1, 8)
-    var largest = arr[0]
-    for (i in 1 until arr.size) {
-        if (arr[i] > largest) {
-            largest = arr[i]
+    val numbers = arrayOf(5, 2, 9, 1, 8)
+    var largest = numbers[0]
+    for (num in numbers) {
+        if (num > largest) {
+           largest = num
         }
     }
     println(largest)
