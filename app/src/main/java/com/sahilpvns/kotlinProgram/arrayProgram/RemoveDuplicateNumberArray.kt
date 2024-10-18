812040b1f6caef89327207e52c8a2be58cf41da6
sahil <sahilpvns@gmail.com>
2024-10-18 21:25:27 +0530

    Program

---
+++ app/src/main/java/com/sahilpvns/kotlinProgram/arrayProgram/RemoveDuplicateNumberArray.kt
@@ -0,0 +1,17 @@
+package com.sahilpvns.kotlinProgram.arrayProgram
+
+
+// Remove duplicate elements from Sorted Array in kotlin
+fun main() {
+    val arr = intArrayOf(1, 1, 2, 2, 3, 4, 4, 5)
+    var j = 0
+    for (i in 0 until arr.size - 1) {
+        if (arr[i] != arr[i + 1]) {
+            arr[j++] = arr[i]
+        }
+    }
+    arr[j++] = arr[arr.size - 1]
+    for (i in 0 until j) {
+        print("${arr[i]} ")
+    }
+}
