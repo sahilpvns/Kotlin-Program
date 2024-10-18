0e94cef19b6ce95196859a3ec8344e7e0e52b3ee
sahil <sahilpvns@gmail.com>
2024-10-18 16:59:43 +0530

    Program

---
+++ app/src/main/java/com/sahilpvns/kotlinProgram/arrayProgram/MissingNumberArray.kt
@@ -0,0 +1,15 @@
+package com.sahilpvns.kotlinProgram.arrayProgram
+
+
+// Missing number in an array
+fun main() {
+    val arr = arrayOf(1, 2, 4, 5, 6)
+    val n = arr.size + 1
+    val expectedSum = n * (n + 1) / 2
+    var actualSum = 0
+    for (num in arr) {
+        actualSum += num
+    }
+    val missingNumber = expectedSum - actualSum
+    println(missingNumber)
+}
