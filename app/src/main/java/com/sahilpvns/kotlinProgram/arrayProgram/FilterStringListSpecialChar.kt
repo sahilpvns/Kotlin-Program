91c91387eda1398d67c39f3b0f0e16f4198f0446
Sahil Kumar <68979700+sahilpvns@users.noreply.github.com>
2024-11-04 08:40:50 +0530

    Update FilterStringListSpecialChar.kt

--- app/src/main/java/com/sahilpvns/kotlinProgram/arrayProgram/FilterStringListSpecialChar.kt
+++ app/src/main/java/com/sahilpvns/kotlinProgram/arrayProgram/FilterStringListSpecialChar.kt
@@ -2,7 +2,7 @@ package com.sahilpvns.kotlinProgram.arrayProgram
 
 // Filter String list containing special char
 fun main() {
-    val list = listOf("car, bike", "bus", "train", "truck")
+    val list = listOf("car", "bike", "bus", "train", "truck")
     val filteredList = list.filter { it.contains("i") }
     println(filteredList)
 
