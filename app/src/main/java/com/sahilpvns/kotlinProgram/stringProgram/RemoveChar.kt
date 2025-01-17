5fba37337d9304a68e78a9288725a60aed2b33f1
sahil <sahilpvns@gmail.com>
2025-01-17 15:40:56 +0530

    Program

---
+++ app/src/main/java/com/sahilpvns/kotlinProgram/stringProgram/RemoveChar.kt
@@ -0,0 +1,11 @@
+package com.sahilpvns.kotlinProgram.stringProgram
+
+// "Helloworld" remove "l" char in kotlin
+fun main() {
+    val str = "Helloworld"
+    val result = str.filter { it != 'l' }
+    println(result)
+}
+
+// Heoword
+
