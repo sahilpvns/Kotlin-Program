fddc6b3e8174b93be9966b5fa924c1d8f9a055a1
sahil <sahilpvns@gmail.com>
2025-01-18 11:05:59 +0530

    Program

--- app/src/main/java/com/sahilpvns/kotlinProgram/stringProgram/RemoveVowelString.kt
+++ app/src/main/java/com/sahilpvns/kotlinProgram/stringProgram/RemoveVowelString.kt
@@ -2,8 +2,8 @@ package com.sahilpvns.kotlinProgram.stringProgram
 
 //Remove Vowel in String
 fun main(){
-    val vowel = "aeiouAEIOU"
     val originalString = "Google"
+    val vowel = "aeiouAEIOU"
     val newString = originalString.filter { it !in vowel }
     println(newString)
 }
