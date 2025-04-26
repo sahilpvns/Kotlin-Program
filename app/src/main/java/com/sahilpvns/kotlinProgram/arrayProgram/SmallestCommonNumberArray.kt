a06e75dc0f7680be7a6c9d8e820213926d27313c
sahil <sahilpvns@gmail.com>
2025-04-26 22:53:34 +0530

    Program

--- app/src/main/java/com/sahilpvns/kotlinProgram/arrayProgram/SmallestCommonNumberArray.kt
+++ app/src/main/java/com/sahilpvns/kotlinProgram/arrayProgram/SmallestCommonNumberArray.kt
@@ -2,7 +2,11 @@ package com.sahilpvns.kotlinProgram.arrayProgram
 
 // Smallest Common Number in array list
 fun main() {
-    val arrays = listOf(listOf(1, 2, 3), listOf(2, 3, 4), listOf(3, 4, 5))
+    val arrays = listOf(
+        listOf(1, 2, 3),
+        listOf(2, 3, 4),
+        listOf(3, 4, 5)
+    )
     val smallestCommonNumber = findSmallestCommonNumber(arrays)
     println(smallestCommonNumber)
 }
